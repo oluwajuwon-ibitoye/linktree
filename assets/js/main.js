c2e962e1cbc8f4b16ddfda741497e752e7c00e5e
Oluwajuwon Ibitoye <144378453+oluwajuwon-ibitoye@users.noreply.github.com>
2025-03-03 15:17:07 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+
+var dob = new Date("05/04/1987");
+var month_diff = Date.now() - dob.getTime();
+var age_dt = new Date(month_diff);
+var year = age_dt.getUTCFullYear();
+var age = Math.abs(year - 1970);
+document.getElementById("age").innerHTML = age;
