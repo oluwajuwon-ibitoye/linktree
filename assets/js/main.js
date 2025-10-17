ba5a3d9703cc88ea0448459c27d147148435d664
Oluwajuwon Ibitoye <144378453+oluwajuwon-ibitoye@users.noreply.github.com>
2025-10-17 09:06:50 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,14 +6,4 @@ var year = age_dt.getUTCFullYear();
 var age = Math.abs(year - 1970);
 document.getElementById("age").innerHTML = age;
 
-<!--Start of Tawk.to Script-->
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/68f105a07690531950dd5229/1j7mnrv71';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-<!--End of Tawk.to Script-->
+
